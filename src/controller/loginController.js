99557915959debf836a4bfd570f86d6ee5cc5433
Luiz Gustavo <l.cgoncalvea@gmail.com>
2022-03-14 19:53:19 -0300

    Correçao do erro de nao selecionar treinador na criaçao de aluno e alteraçoes na escrita e layout do index

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -3,92 +3,97 @@ const Treinador = require('../models/Treinador');
 const User = require('../models/User')
 const bcrypt = require('bcryptjs');
 
-exports.index = (req,res) => {
+exports.index = (req, res) => {
     res.render('login');
 }
-exports.login = async (req,res) => {
-    try{
-        const treinador = await Treinador.findOne({where: { email: req.body.email}});
-        const aluno = await Aluno.findOne({where:{ email : req.body.email}});
+exports.login = async (req, res) => {
+    try {
+        const treinador = await Treinador.findOne({ where: { email: req.body.email } });
+        const aluno = await Aluno.findOne({ where: { email: req.body.email } });
         let userIsTreinador
-        if(!treinador && !aluno){
-            req.flash('errors','Usuario Nao encontrado');
-            req.session.save(()=>{
+        if (!treinador && !aluno) {
+            req.flash('errors', 'Usuario Nao encontrado');
+            req.session.save(() => {
                 return res.redirect('/login')
             })
             return
         }
-        if(treinador){
+        if (treinador) {
             user = treinador
             userIsTreinador = true;
         }
-        if(aluno){
+        if (aluno) {
             user = aluno
             userIsTreinador = false;
         }
-        if(!bcrypt.compareSync(req.body.password,user.hashPassword) ){
-            req.flash('errors','Usuario ou senha incorretos')
-            req.session.save(()=>{
+        if (!bcrypt.compareSync(req.body.password, user.hashPassword)) {
+            req.flash('errors', 'Usuario ou senha incorretos')
+            req.session.save(() => {
                 return res.redirect('/login')
             })
             return;
         }
         req.session.user = user;
         req.session.userIsTreinador = userIsTreinador
-            req.session.save(()=>{
-              return  res.redirect('/');
-            })
-            return;
+        req.session.save(() => {
+            return res.redirect('/');
+        })
+        return;
 
-    }catch(e){
+    } catch (e) {
         console.log(e)
         res.send(req.body)
     }
 }
-exports.logout = async (req,res) => {
-    try{
+exports.logout = async (req, res) => {
+    try {
         req.session.destroy();
         res.redirect('/login')
-    }catch(e){
+    } catch (e) {
         console.log('Erro ao deletar o Usuario');
     }
 }
-exports.registerIndex = (req,res) => {
+exports.registerIndex = (req, res) => {
     res.render('register');
 }
 
-exports.register = async (req,res) => {
-    try{
-        if(await (await Treinador.findAll({where:{email: req.body.email}})).length > 0 || await Aluno.findAll({where:{email:req.body.email}}) > 0){
-            req.flash('errors',['Email Já Utilizado'])
-            req.session.save(function(){
+exports.register = async (req, res) => {
+    try {
+        if (await (await Treinador.findAll({ where: { email: req.body.email } })).length > 0 || await Aluno.findAll({ where: { email: req.body.email } }) > 0) {
+            req.flash('errors', ['Email Já Utilizado'])
+            return req.session.save(function () {
                 return res.redirect('/register')
             });
-            return;
         }
 
-        if(req.body.userType === 'on'){
+        if (req.body.userType === 'on') {
             const novoTreinador = await Treinador.create(req.body);
-            req.flash('sucess',['Treinador Cadastado com sucesso'])
-            req.session.save(()=>{
-               return res.redirect('/');
+            req.flash('sucess', ['Treinador Cadastado com sucesso'])
+            return req.session.save(() => {
+                return res.redirect('/login');
             })
-            return;
         }
-        if(req.body.userType === undefined){
+        console.log(req.body.treinador_id);
+        if (req.body.treinador_id === "Selecione o seu Treinador") {
+            req.flash('errors', ['Treinador nao Selecionado']);
+           return req.session.save(() => {
+                return res.redirect('/register');
+            })
+        }
+        if (req.body.userType === undefined) {
             const novoAluno = await Aluno.create(req.body);
-            req.flash('sucess',['Aluno Cadastado com sucesso'])
-            req.session.save(()=>{
-               return res.redirect('/');
+            req.flash('sucess', ['Aluno Cadastado com sucesso'])
+            req.session.save(() => {
+                return res.redirect('/');
             })
             return;
         }
-    }catch(e){
+    } catch (e) {
         e.errors.forEach(erro => {
-            req.flash('errors',erro.message)
+            req.flash('errors', erro.message)
         });
-        req.session.save(()=>{
+        req.session.save(() => {
             return res.redirect('/register');
-         })
+        })
     }
 }
